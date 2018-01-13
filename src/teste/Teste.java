1ffd515822202b875316be5d1e83e7834515670d
Lauro <skidrj@hotmail.com>
2018-01-13 12:13:01 -0200

    Atualizando

--- src/teste/Teste.java
+++ src/teste/Teste.java
@@ -6,21 +6,21 @@ public class Teste {
 
 	public static void main(String[] args) {
 		/* 
+		 * EX:
+		 * "Um pequeno jabuti xereta viu dez cegonhas felizes"
 		 * Opção Com JoptionPane
 		 * */
-		// String string = JOptionPane.showInputDialog(null, "Digite a Frase que deseja quebrar");
-		// int coluna = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a quantidade de colunas"));
-		
-		
-		String string = "Um pequeno jabuti xereta viu dez cegonhas felizes teste teste teste teste teste teste teste teste";
-		/*
-		 * Opção Direta Inserindo os Valores diretamente
-		 */
-		int coluna = 20;
-		
+		String string = JOptionPane.showInputDialog(null, 
+				"Digite a Frase que deseja quebrar");
+		int coluna = Integer.parseInt(JOptionPane.showInputDialog(null, 
+				"Digite a quantidade de colunas"));
+		//aux - resgata a coluna inserindo novamente no if o valor original.
 		int aux = coluna;
+		//Inicia um contador que confere a coluna para menos e quebra a linha.
 		int contador = 0;
+		// um array para ler a string e separar no for
 		char[] charArray = string.toCharArray();
+		//StringBuilder para refazer a frase quebrando em linhas.
 		StringBuilder stb = new StringBuilder();
 		for (int i = 0; i < charArray.length; i++) {
 			if(charArray[i] == ' ' & contador > coluna) {				
@@ -28,16 +28,11 @@ public class Teste {
 				contador = 0;
 				coluna = aux;
 			}else {
-				//System.out.println(charArray[i]);
 				coluna--;
 				contador++;
 				stb.insert(0, charArray[i]);
-				//System.out.println(stb);
 			}
-			
 		}
 		System.out.println(stb.reverse());	
 	}
 }
-
-
